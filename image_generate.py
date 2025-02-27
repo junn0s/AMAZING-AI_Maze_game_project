af098936a07915d49d48c7329459710769ea373f
J <163096837+junn0s@users.noreply.github.com>
2025-02-28 06:50:52 +0900

    Update image_generate.py

--- image_generate.py
+++ image_generate.py
@@ -1,19 +1,22 @@
 import os
-import openai
+from openai import OpenAI
 from dotenv import load_dotenv
 load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI()
 
 def generate_image(prompt: str, n: int = 1, size: str = "1024x1024") -> str:
     try:
         # 이미지 생성 요청
-        response = openai.Image.create(
-            prompt=prompt,
+        response = client.images.generate(
+            model = "dall-e-3",
+            prompt = prompt,
             n=n,
             size=size
         )
         # 응답에서 첫 번째 이미지의 URL 추출
-        image_url = response['data'][0]['url']
+        image_url = response.data[0].url
+        #image_url = response['data'][0]['url']
         return image_url
     except Exception as e:
         print("이미지 생성 중 오류 발생:", e)
